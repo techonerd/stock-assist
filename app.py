7d5110eee635d8632cc2740bed3bc1c3205367e8
Ranjeet Singh <techonerd1.0@gmail.com>
2022-10-04 03:11:04 +0530

    app init

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Predicting the Stocks Price')
+st.markdown('Search your favourite stock and predict the price for the next 30 days')
