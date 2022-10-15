52501e3c13573598c61df3fad8270be5286e4d5d
Amit Singh <amitjordanthegr8@gmail.com>
2022-10-15 17:25:40 +0530

    Update model.py
    added get function

--- model.py
+++ model.py
@@ -1,14 +1,20 @@
 import requests
 import pandas as pd
 
-#define this function to get user input for a time frame
-# def get_function(function):
-#     if 'intraday' in (x.lower):
-#         return 'TIME_SERIES_INTRADAY'
-#     elif 'intraday extended' in (x.lower):
-#         return 'TIME_SERIES_INTRADAY_EXTENDED'
-#     elif
-#     return function
+# define this function to get user input for a time frame
+def get_function(function):
+    if 'intraday' in (x.lower):
+        return 'TIME_SERIES_INTRADAY'
+    elif 'intraday extended' in (x.lower):
+        return 'TIME_SERIES_INTRADAY_EXTENDED'
+    elif 'daily' in (x.lower):
+      return 'TIME_SERIES_DAILY'
+    elif 'weekly' in (x.lower):
+      return 'TIME_SERIES_WEEKLY'
+    elif 'monthly' in (x.lower):
+      return 'TIME_SERIES_MONTHLY'
+    else:
+      return (x.UPPER)
   
 #define this function to get user input for a symbol
 #def get_symbol(symbol):
@@ -30,7 +36,7 @@ import pandas as pd
 symbol=input('Get Symbol')
 interval=input('Get interval')
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?'+'function={}'+'&symbol={}'+'&interval={}'+'&apikey=4BB9V8QME0K5H135'
+url = 'https://www.alphavantage.co/query?'+'function={}'+'&symbol={}'+'&interval={}'+'&apikey=4BB9V8QME0K5H135'.format(function,symbol,interval)
 r = requests.get(url)
 data = r.json()
 df=pd.json_normalize(data)
