bc375983f982ab94079aefd63ade6946dc9425c7
Casper-Morning-Star <casperdmorningstar@gmail.com>
2022-10-31 01:01:19 +0530

    update about page
    added basic content for initial phase of about page.

---
+++ pages/01_About.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="About",
+    page_icon="	ℹ️",
+)
+
+st.write("# Welcome to Stock Assist! 👋")
+
+# st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Stock Assist is an AI based prediction app built for
+    providing prediction related to stocks based on ML/AI.
+
+    ### Want to learn more?
+    - Check out [stock-assist](bit.ly/stock-assist)
+    - Jump into our [documentation](#todo)
+    - Ask a question in our [community
+        ](#todo)
+"""
+)
