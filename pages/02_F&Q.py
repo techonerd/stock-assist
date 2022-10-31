da721b177cef91fde1c8647a8c8ded385a1ab744
Casper-Morning-Star <casperdmorningstar@gmail.com>
2022-10-31 18:59:37 +0530

    updat f&q.py
    created f&q page for our app

--- pages/02_F&Q.py
+++ pages/02_F&Q.py
@@ -1,9 +1,33 @@
 import streamlit as st
 
-display = ("male", "female")
+st.set_page_config(
+    page_title="F&Q",
+    page_icon="❓",
+)
 
-options = list(range(len(display)))
+st.write("# Welcome to Stock Assist F&Q! 👋")
+body =   """
+            <details>
+                <summary>What is stock assist?</summary>
+                Stock Assist is an AI based prediction app built for providing prediction related to stocks based on ML/AI.
+            </details>
+            
+            <details>
+                <summary>How to get started with stock assist?</summary>
+                You can refer our <a href="https://github.com/techonerd/stock-assist/blob/main/CONTRIBUTING.md"> contribution </a> guide. 
+                Also refer to our <a href="https://github.com/techonerd/stock-assist/blob/main/CODE_OF_CONDUCT.md"> code of conduct </a> for comunity guidlines.
+            </details>
+            
+            <details>
+                <summary>Where can I finds relevant documents??</summary>
+                Not sure what your looking for, head towards our <a href="https://github.com/techonerd/stock-assist/blob/main/getting_started.md"> getting started doc's </a>
+            </details>
+            
+            <details>
+                <summary>How to get help?</summary>
+                Feel free to join us on discord or mail us at <a href="mailto:info.stockassist@gmail.com">info.stockassist@gmail.com</a>
+            </details>
+            """
 
-value = st.selectbox("gender", options, format_func=lambda x: display[x])
 
-st.write(value)
+st.markdown(body, unsafe_allow_html=True)
